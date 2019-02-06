85bfe919027489e4f7825a192e96a5e1a7a2f0eb
SOURADIP PANJA <souradippanja2@gmail.com>
2019-02-06 23:20:48 +0530

    Navbae

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import AppNavbar from "./components/layout/AppNavbar";
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
+      <Router>
       <div className="App">
+        <AppNavbar />
         <h1>Souradip</h1>
       </div>
+      </Router>
     );
   }
 }
